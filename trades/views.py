5b16cac16d30c4eb06d5274465c57a46b323b619
kidus <Kidkasam@users.noreply.github.com>
2026-02-19 16:10:52 +0300

    Add TradeViewSet with authenticated user filtering

--- trades/views.py
+++ trades/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Trade
+from .serializers import TradeSerializer
 
-# Create your views here.
+class TradeViewSet(viewsets.ModelViewSet):
+    serializer_class = TradeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Trade.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
