d7b83292353559235219d145e1472b88fdbdf094
kidus <Kidkasam@users.noreply.github.com>
2026-02-24 11:53:47 +0300

    Implement user authentication APIs (register, login, logout)

--- trades/serializers.py
+++ trades/serializers.py
@@ -1,5 +1,22 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Trade, Strategy, AnalyticsSnapshot
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            email = validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 class TradeSerializer(serializers.ModelSerializer):
     class Meta:
--- trades/views.py
+++ trades/views.py
@@ -1,6 +1,9 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from django.contrib.auth import authenticate, login, logout
+from rest_framework.views import APIView
+
 from .models import Trade, Strategy, AnalyticsSnapshot
-from .serializers import TradeSerializer, StrategySerializer, AnalyticsSnapshotSerializer
+from .serializers import TradeSerializer, StrategySerializer, AnalyticsSnapshotSerializer, UserRegisterSerializer
 
 class TradeViewSet(viewsets.ModelViewSet):
     queryset = Trade.objects.all()
@@ -34,3 +37,23 @@ class AnalyticsSnapshotViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+class  RegisterAPIView(APIView):
+    def post(self, request):
+        serializer = UserRegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class LoginAPIView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            return Response({"message": "Logged in successfully"})
+        return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+class LogoutAPIView(APIView):
+    def post(self, request):
+        logout(request)
+        return Response({"message": "Logged out successfully"})
