0b902ac050477b06440c3ec6316aec95b2404c90
kidus <Kidkasam@users.noreply.github.com>
2026-02-25 14:47:06 +0300

    add filters.py

--- edgeforge/settings.py
+++ edgeforge/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'trades',
     'rest_framework',
     'rest_framework.authtoken',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -61,6 +62,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
 }
 
 
