4fa0899557e5488ebd43dcafe16f99779badee15
kidus <Kidkasam@users.noreply.github.com>
2026-02-25 14:47:17 +0300

    add filters.py

---
+++ trades/filters.py
@@ -0,0 +1,19 @@
+import django-filters
+from . models import Trade
+
+class TradeFilter(django_filters.FilterSet):
+    start_date = django_filters.DateFilter(fieldname='trade_date', lookup_exp='gte')
+    end_date = django_filters.DateFilter(fieldname='trade_date', lookup_exp='lte')
+    winning = django_filters.BooleanFilter(method='filter_winning')
+
+
+    class Meta:
+        models= Trade
+        fields = ['buy_Sell', 'trading_session', 'market_pair']
+
+    def filter_winning(self, queryset, name, value):
+        if value is True:
+            return queryset.filter(profit_loss__gt=0)
+        elif value is False:
+            return queryset.filter(profit_loss__lte=0)
+        return queryset
